5a233607c4cf9e03f9a7764f11fe728c0bd22f4d
Oscar302 <osma22ad@student.cbs.dk>
2023-09-20 08:02:23 +0200

    Hello

--- server.js
+++ server.js
@@ -1,34 +1,14 @@
 
 const express = require("express");
-const bodyParser = require('body-parser');
-const cors = require("cors");
 
 const app = express();
 const port = 3001
 
-app.use(cors());
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-
-let id = 0;
-const db = [{id: 1, username : "Oscar", password : "test", email : "hej.com"}];
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-})
-.post('/create-user', (req, res) => {
-  let payload = req.body;
-  console.log(payload);
-  db.push(payload);
-  res.send(db);
-})
-.get("/:id", (req, res) => {
-  
-})
-.delete("/:id", (req, res) =>{
-})
+app.get('/', (req, res) =>  {
+  res.send("Hello Mate!")
+  }
+  )
 
 app.listen(port, () => {
-  console.log(`Server open on port ${port}`);
+console.log(`Server open on port ${port}`);
 });
- 
