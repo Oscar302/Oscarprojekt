0ebc3264e70632ffae75800b6d6800afb3e1cc9a
Oscar302 <osma22ad@student.cbs.dk>
2023-09-20 10:05:27 +0200

    helloooo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 
 const app = express();
-const port = 3000
+const port = 3001
 
 app.get('/', (req, res) =>  {
   res.send("Hello Mate!")
@@ -12,3 +12,4 @@ app.get('/', (req, res) =>  {
 app.listen(port, () => {
 console.log(`Server open on port ${port}`);
 });
+
